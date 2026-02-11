f86c395712ddc10b847cd01d44054319959cd5e8
Hafidz Fauzi <masozi395@gmail.com>
2026-02-11 16:52:54 +0700

    first commit

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mahasiswa;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $mahasiswa = Mahasiswa::all();
+        return view('mahasiswa.index', compact('mahasiswa'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('mahasiswa.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'nim' => 'required|unique:mahasiswas',
+            'nama' => 'required',
+            'prodi' => 'required',
+            'angkatan' => 'required',
+            'email' => 'required|email|unique:mahasiswas'
+        ]);
+
+        Mahasiswa::create($validatedData);
+
+        return redirect()->route('mahasiswa.index')
+        ->with('success', 'Mahasiswa created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Mahasiswa $mahasiswa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Mahasiswa $mahasiswa)
+    {
+        return view('mahasiswa.edit', compact('mahasiswa'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Mahasiswa $mahasiswa)
+    {
+        $validatedData = $request->validate([
+            'nim' => 'required|unique:mahasiswas,nim,' . $mahasiswa->id,
+            'nama' => 'required',
+            'prodi' => 'required',
+            'angkatan' => 'required',
+            'email' => 'required|email|unique:mahasiswas,email,' . $mahasiswa->id
+        ]);
+
+        $mahasiswa->update($validatedData);
+
+        return redirect()->route('mahasiswa.index')
+        ->with('success', 'Mahasiswa updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Mahasiswa $mahasiswa)
+    {
+        $mahasiswa->delete();
+
+        return redirect()->route('mahasiswa.index')
+        ->with('success', 'Mahasiswa deleted successfully.');
+    }
+}
---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <div class="max-w-7xl mx-auto py-6 px-4">
+
+        {{-- Header --}}
+        <div class="flex justify-between items-center mb-6">
+            <h1 class="text-2xl font-bold text-grey-800">
+                Data Mahasiswa
+            </h1>
+        </div>
+    </div>
+</x-app-layout>
